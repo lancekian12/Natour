68e4beb2549b53cae3e709650b98b1ef37b7bc08
Lance Kian <lafa.flores.up@phinmaed.com>
2024-04-21 23:28:27 +0800

    added mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose.connect();
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
