ff445a18907d6def38fd6d6890612b1c6ae3d191
Lance Kian <lafa.flores.up@phinmaed.com>
2024-04-22 02:09:04 +0800

    Added mongoose connect

--- server.js
+++ server.js
@@ -11,14 +11,45 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  // just some options to deal with deprecation warnings.
   .connect(DB, {
-    useNewURLParser: true,
+    useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
   })
-  .then(() => console.log('DB connection established'));
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker3',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR', err);
+  });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
