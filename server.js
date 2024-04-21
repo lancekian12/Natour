35e1029c83acaf0793d6a1210199f6805a5f113b
Lance Kian <lafa.flores.up@phinmaed.com>
2024-04-22 01:07:43 +0800

    Added files

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-mongoose.connect();
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // just some options to deal with deprecation warnings.
+  .connect(DB, {
+    useNewURLParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection established'));
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
